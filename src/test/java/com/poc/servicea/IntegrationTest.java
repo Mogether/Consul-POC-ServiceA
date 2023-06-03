87064fdd6fdcbda2bd6016c3867f9a400d751dd7
ImKunYoung <ur2kunyoung2@outlook.com>
2023-06-03 16:16:37 +0900

    Initial version of ServiceA generated by generator-jhipster@7.9.3

---
+++ src/test/java/com/poc/servicea/IntegrationTest.java
@@ -0,0 +1,24 @@
+package com.poc.servicea;
+
+import com.poc.servicea.ServiceAApp;
+import com.poc.servicea.config.AsyncSyncConfiguration;
+import com.poc.servicea.config.EmbeddedKafka;
+import com.poc.servicea.config.EmbeddedSQL;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = { ServiceAApp.class, AsyncSyncConfiguration.class })
+@EmbeddedKafka
+@EmbeddedSQL
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
+public @interface IntegrationTest {
+}
